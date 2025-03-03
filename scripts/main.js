763d9bee2f85abdae56ec6cf1010320d3c9cd556
Nela <nw2023@hw.ac.uk>
2025-03-03 16:16:04 +0000

    add shared colour + region functions in main

---
+++ scripts/main.js
@@ -0,0 +1,25 @@
+const colours = ["#cb55a5","#66b743","#8345ca","#b19e42","#685699","#d27131","#6999c2","#d24049","#64b088","#763535","#4b612e"]
+const regions = [
+    {name: "Scotland", code: "S92000003"},
+    {name: "North East", code: "E12000001"},
+    {name: "North West", code: "E12000002"},
+    {name: "Yorkshire and The Humber", code: "E12000003"},
+    {name: "East Midlands", code: "E12000004"},
+    {name: "West Midlands", code: "E12000005"},
+    {name: "East", code: "E12000006"},
+    {name: "London", code: "E12000007"},
+    {name: "South East", code: "E12000008"},
+    {name: "South West", code: "E12000009"},
+]
+
+function regionNameToCode(regionName) {
+    return regions.find(d => d.name === regionName).code
+}
+
+function regionCodeToName(regionCode) {
+    return regions.find(d => d.code === regionCode).name
+}
+
+const regionScaleByName = d3.scaleOrdinal()
+    .domain(regions.map(d => d.name).sort())
+    .range(colours)
