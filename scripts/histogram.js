99c39afdcf4004ac7b7e0f1076d1f85c5870c80d
Nela <nw2023@hw.ac.uk>
2025-03-25 17:22:17 +0000

    histogram: add histogram to page

---
+++ scripts/histogram.js
@@ -0,0 +1,81 @@
+const dataFile = "/data/likelihood_of_moving_by_age.json";
+
+d3.json(dataFile).then(data => {
+
+   
+    //a.push(likelihood_of_moving_by_age[0].ages.find(d => d.age == "90+"))
+  
+    const width = 960;
+    const height = 500;
+    const marginTop = 20;
+    const marginRight = 20;
+    const marginBottom = 30;
+    const marginLeft = 60;
+
+    const svg = d3.select("svg#histogram")
+        .attr("width", width)
+        .attr("height", height)
+        .attr("viewBox", [0, 0, width, height])
+        .attr("style", "max-width: 100%; height: auto;");
+
+    const barGroup = svg.append("g")
+        .style("fill", "steelblue");
+
+    const xAxisGroup = svg.append("g")
+        .attr("transform", `translate(0,${height - marginBottom})`)
+        .call(g => g.append("text")
+                .attr("x", width)
+                .attr("y", marginBottom - 4)
+                .attr("fill", "currentColor")
+                .style("font-weight", "bold")
+                .attr("text-anchor", "end")
+                .text("Age →"));
+
+    const yAxisGroup = svg.append("g")
+        .attr("transform", `translate(${marginLeft},0)`)
+        .call(g => g.append("text")
+                .attr("x", -marginLeft)
+                .attr("y", 10)
+                .attr("fill", "currentColor")
+                .attr("text-anchor", "start")
+                .text("↑ Frequency (no. of moves)"));
+
+    function update(year) {
+        const bins = d3.bin().value(d => d.age).thresholds(20)(data.count.find(d => d.year === year).ages)
+    
+        // Declare the x (horizontal position) scale.
+        const x = d3.scaleLinear()
+            .domain([bins[0].x0, bins[bins.length - 1].x1])
+            .range([marginLeft, width - marginRight]);
+    
+        // Declare the y (vertical position) scale.
+        const y = d3.scaleLinear()
+            .domain([0, d3.max(bins, d => d3.sum(d, e => e.value))])
+            .range([height - marginBottom, marginTop]);
+    
+        // Add a rect for each bin.
+        barGroup.selectAll(".bin")
+            .data(bins)
+            .join("rect")
+            .classed("bin", true)
+            .attr("x", d => x(d.x0) + 1)
+            .attr("width", d => x(d.x1) - x(d.x0) - 1)
+            .transition()
+            .attr("y", d => y(d3.sum(d, e => e.value)))
+            .attr("height", d => y(0) - y(d3.sum(d, e => e.value)));
+    
+        // Add the x-axis and label.
+        xAxisGroup.transition()
+            .call(d3.axisBottom(x).ticks(width / 80).tickSizeOuter(0));
+    
+        // Add the y-axis and label, and remove the domain line.
+        yAxisGroup.transition()
+            .call(d3.axisLeft(y).ticks(height / 40))
+            
+    }
+
+    update(Number(sliderCurrentValue()))
+    sliderRegisterCallback(function () {
+        update(Number(this.value))
+    })
+})
